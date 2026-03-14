0d26fda36504ee151c74ec29f98616875966c622
ianwarsys <ian.avg07@gmail.com>
2026-03-14 14:17:16 -0300

    Classes user and OrderStatus created, H2 configured in the profile 'test', H2 client web tested by mapping User class with ORM annotations

---
+++ src/main/java/com/dev_ian/dscommerce/DscommerceApplication.java
@@ -0,0 +1,14 @@
+package com.dev_ian.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+}
