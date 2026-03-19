eb660ef00cd457c90d616363b0873ef34897e045
ian-avila <ian.avg07@gmail.com>
2026-03-19 14:32:14 -0300

    Create OrderItem association class and OrderItemPK with composite primary key from Product and Order entities

--- src/main/java/com/dev_ian/dscommerce/entities/Order.java
+++ src/main/java/com/dev_ian/dscommerce/entities/Order.java
@@ -4,6 +4,8 @@ import com.dev_ian.dscommerce.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -22,6 +24,10 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
+
 
     public Order() {
     }
@@ -72,5 +78,9 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 }
